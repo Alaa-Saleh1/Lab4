9f5d284e2faf1074322232e67d887cb00912ca50
Ala'a <alaa.salenazi@gmail.com>
2024-11-07 11:32:20 +0300

    Add files via upload

---
+++ Lab4/Shape.java
@@ -0,0 +1,27 @@
+package Lab4;
+
+public abstract class Shape {
+
+    private int x;
+    private int y;
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public abstract double calculateArea();
+    public abstract double calculateCircumference();
+}
